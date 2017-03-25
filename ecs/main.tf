eaf6819e01c149fe1f87dfdb6291118c37d36c21
Rustam Arslanov <rustamar@gmail.com>
2017-03-25 20:34:05 +0200

    Added credentials

--- ecs/main.tf
+++ ecs/main.tf
@@ -4,3 +4,8 @@ provider "aws" {
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
 }
+
+resource "aws_key_pair" "admin" {
+  key_name   = "admin"
+  public_key = "ssh-rsa MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmRKwi6zbhzrg+tEl1O9c2LiHACFN+/j3ZgDKe2/5J4qKlGTmc8KUzSSwO9LApPAeHu0efsxCCzSXHr9YfSgt2Bg1j7ppxdLlxPMmtLf3i2gROmY/CcV/wUOnZ+L6O3PXpKz2XT0iwjbTlCG/NO9NvHuQxYE01hVDHjdEZiXBtRBurwFP7aXZ2XgxbCEMgQhLJhkTXPhyNQMDtw/yo/i4eETnKvKmt7DdTZP+hUMMXhRvwKsoXPIKPYtH82G1eXPtv19O1EnZUDIPUy+ZR34ZP66gxxzi8/xKkZOH6FRAYkmoXfZWAQrpngosay1AbhlX+5TfuAFgU2AB2ig8rzb7hQIDAQAB"
+}
